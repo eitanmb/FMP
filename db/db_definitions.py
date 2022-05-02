9b339e05cc2a2282ca62fa6ce1246191c271ddce
Eitan Mizrachi Belisario <eitanmb@gmail.com>
2022-05-02 14:21:09 -0400

    Indexes structures created

--- db/db_definitions.py
+++ db/db_definitions.py
@@ -7,26 +7,26 @@ TABLE_PROFILE_STRUCTURE = f'CREATE TABLE profile ( \
         `volAvg` varchar(50) DEFAULT NULL, \
         `mktCap` varchar(50) DEFAULT NULL, \
         `lastDiv` varchar(50) DEFAULT NULL, \
-        `range` text, \
-        `changes` text DEFAULT NULL, \
-        `companyName` text, \
+        `range` varchar(250) DEFAULT NULL, \
+        `changes` varchar(250) DEFAULT NULL, \
+        `companyName` varchar(250) DEFAULT NULL, \
         `currency` text, \
         `cik` varchar(100) DEFAULT NULL, \
         `isin` text, \
         `cusip` text DEFAULT NULL, \
         `exchange` text, \
         `exchangeShortName` text, \
-        `industry` text, \
-        `website` text, \
+        `industry` varchar(250) DEFAULT NULL, \
+        `website` varchar(250) DEFAULT NULL, \
         `description` text, \
         `ceo` text, \
-        `sector` text, \
-        `country` text, \
+        `sector` varchar(250) DEFAULT NULL, \
+        `country` varchar(250) DEFAULT NULL, \
         `fullTimeEmployees` varchar(20) DEFAULT NULL, \
         `phone` varchar(100) DEFAULT NULL, \
         `address` text, \
-        `city` text, \
-        `state` text, \
+        `city` varchar(250) DEFAULT NULL, \
+        `state` varchar(250) DEFAULT NULL, \
         `zip` varchar(100) DEFAULT NULL, \
         `dcfDiff` varchar(50) DEFAULT NULL, \
         `dcf` varchar(50) DEFAULT NULL, \
@@ -39,3 +39,36 @@ TABLE_PROFILE_STRUCTURE = f'CREATE TABLE profile ( \
         `isFund` tinyint(1) DEFAULT NULL, \
         PRIMARY KEY (`id`) \
       ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;'
+
+
+PROFILE_INDEXES = f'ALTER TABLE `profile` \
+                    ADD INDEX `ISector` (`sector` ASC) VISIBLE, \
+                    ADD INDEX `ICountry` (`country` ASC) VISIBLE, \
+                    ADD INDEX `IIndustry` (`industry` ASC) VISIBLE, \
+                    ADD INDEX `ICompany` (`companyName` ASC) VISIBLE, \
+                    ADD INDEX `ISecInd` (`sector` ASC, `industry` ASC) VISIBLE, \
+                    ADD INDEX `ISecIndCtry` (`industry` ASC, `sector` ASC, `country` ASC) VISIBLE, \
+                    ADD INDEX `ICompSect` (`companyName` ASC, `sector` ASC) VISIBLE, \
+                    ADD INDEX `ICompInd` (`companyName` ASC, `industry` ASC) VISIBLE, \
+                    ADD INDEX `ICompIndSec` (`companyName` ASC, `sector` ASC, `industry` ASC) VISIBLE;'
+
+
+IS_INDEXES = f'ALTER TABLE `incomeStatement` \
+              ADD INDEX `IRevenue` (`revenue` ASC) VISIBLE, \
+              ADD INDEX `ICostOfRevenue` (`costOfRevenue` ASC) VISIBLE, \
+              ADD INDEX `IOperatingExpenses` (`operatingExpenses` ASC) VISIBLE, \
+              ADD INDEX `IRevCostExpDate` (`revenue` ASC, `costOfRevenue` ASC, `operatingExpenses` ASC, `calendarYear` ASC) VISIBLE, \
+              ADD INDEX `IDate` (`calendarYear` ASC) VISIBLE;'
+
+BS_INDEXES = f'ALTER TABLE `balanceSheet` \
+              ADD INDEX `IDate` (`calendarYear` ASC) VISIBLE, \
+              ADD INDEX `IAssLiabInv` (`totalLiabilities` ASC, `totalAssets` ASC, `inventory` ASC) VISIBLE, \
+              ADD INDEX `IAsstes` (`totalAssets` ASC) VISIBLE, \
+              ADD INDEX `ILiab` (`totalLiabilities` ASC) VISIBLE, \
+              ADD INDEX `IInventory` (`inventory` ASC) VISIBLE;'
+
+
+
+
+
+
--- mongo/create_mongo_collections.py
+++ mongo/create_mongo_collections.py
@@ -11,9 +11,9 @@ from get_mongo_db import get_database
 
 # Get the database
 BASE_FOLDER: str = DIRS['CURRENT_JSON_FOLDER']
-folder: str      = f"/home/eitan/Programacion/FMP_2/json/2021/09/outlook"
+folder: str      = f"/home/eitan/Programacion/FMP_2/json/2022/04/outlook"
 dbname = get_database()
-collection_name = dbname['outlook_2021_09']
+collection_name = dbname['outlook_2022_04']
 
 print('folder: ', folder)
 
@@ -36,5 +36,16 @@ def create_collection_from_json( folder, collection ):
         except Exception as e:
             print(e)
 
+def create_text_indexes( collection_name ):
+
+    collection_text_indexes = collection_name.financialsAnnual.income.create_index({
+        date:"text",
+        calendarYear:"text",
+        symbol:"text"
+    })
+
+
 
 create_collection_from_json( folder, collection_name)
+
+# create_text_indexes( collection_name )
