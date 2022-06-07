b217ecb4b000cfdb9e6ff9dde0579cb26824e74f
Eitan Mizrachi Belisario <eitanmb@gmail.com>
2022-06-07 10:01:50 -0400

    outlook refactored

--- app/company_outlook.py
+++ app/company_outlook.py
@@ -10,24 +10,19 @@ sys.path.append("..")
 def init() -> None:
 
     BASE_FOLDER = DIRS['CURRENT_JSON_FOLDER']
-    folder: str = f"{BASE_FOLDER}/outlook"
-    data_name: str = "Company Outlook"
     tickers_file = TICKERS_PATH['tickers_financial_info']
     tickers_list: list = FmpAPI.get_tickers_list(tickers_file)
-
     outlook: object = {
         'domain': 'outlook',
         'tickers_list': tickers_list,
         'endpoint': ENDPOINTS['outlook'],
-        'folder': folder
+        'folder': f"{BASE_FOLDER}/outlook"
     }
 
+    data_name: str = "Company Outlook"
     util.print_messages(util.set_init_time(data_name))
-
     FmpAPI.download_companies_data(outlook)
-
-    util.print_messages(util.set_init_time(data_name),
-                        util.set_end_time(data_name))
+    util.print_messages(util.set_end_time(data_name))
 
 
 if __name__ == "_main__":
