fee5071a4fe25128244365343b1610bab446987c
Eitan Mizrachi Belisario <eitanmb@gmail.com>
2022-07-06 16:17:43 -0400

    forex endpoint fixed

--- config/endpoints.py
+++ config/endpoints.py
@@ -5,7 +5,7 @@ ENDPOINTS = {
     "CF"            : "{url_base}/v3/cash-flow-statement/{ticker}?apikey={apikey}",
     "holders"       : "{url_base}/v3/institutional-holder/{ticker}?apikey={apikey}",
     "profile"       : "{url_base}/v3/profile/{ticker}?apikey={apikey}",
-    "forex"         : "{url_base}/v3/historical-price-full/{ticker}&apikey={apikey}",
+    "forex"         : "{url_base}/v3/historical-price-full/{ticker}?apikey={apikey}",
     "floatshares"   : "{url_base}/v4/shares_float?symbol={ticker}&apikey={apikey}",
     "financial_list": "{url_base}/v3/financial-statement-symbol-lists?apikey={apikey}",
     "tradeble_list" : "{url_base}/v3/available-traded/list?apikey={apikey}",
