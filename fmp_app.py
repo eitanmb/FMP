44a3e1495636dabb47448492b98eaaf2cc8a441f
Eitan Mizrachi Belisario <eitanmb@gmail.com>
2022-08-16 21:03:17 -0400

    lastTicker updated

--- fmp_app.py
+++ fmp_app.py
@@ -91,7 +91,3 @@ def init(DIRS):
         print_messages("NoSql executions on:", data['current'])
         if data['kwargs']['noSql'] is not None:
              create_data_persistence_noSQL(data['kwargs'])
-
-   
-if __name__ == '__main__':
-    init()
